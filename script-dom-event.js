164d2d9cb71f6368ca6edaa030f69e8821a9ff24
hendhendy <hendy.maxv@gmail.com>
2021-03-02 14:12:12 +0700

    Guess my number is Done

--- script-dom-event.js
+++ script-dom-event.js
@@ -11,9 +11,11 @@
 // console.log(document.querySelector('.guess').value);
 /*  */
 
-// // Handling Click Events & Implementing the Game Logic // //
-const secretNumber = Math.trunc(Math.random() * 10) + 1;
+// // Handling Click Events & Implementing the Game Logic & Manipulating CSS Style // //
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.number').textContent = secretNumber;
 
@@ -22,10 +24,25 @@ document.querySelector('.check').addEventListener('click', function () {
 
   console.log(guess, typeof guess);
 
+  // When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = 'MASUKIN ANGKA CUK!🚫';
+
+    // When the Player Wins
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = 'Tumben pintar, TEBAK LAGI YUK😛';
+    document.querySelector('.message').textContent =
+      'Tumben pintar, TEBAK LAGI YUK😛';
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+        highscore = score;
+        document.querySelector('.highscore').textContent = highscore;
+    }
+
+    // When the Guess is too High
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Ketinggian woi!';
@@ -36,6 +53,8 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.number').textContent = secretNumber;
       document.querySelector('.score').textContent = 0;
     }
+
+    // When the Guess is too Low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📉 Kerendahan woi!';
@@ -45,12 +64,25 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'Kurang Beruntung!😣';
       document.querySelector('.number').textContent = secretNumber;
       document.querySelector('.score').textContent = 0;
-    } 
+    }
   }
 });
 
 // // Reload Pages
 document.querySelector('.again').addEventListener('click', function () {
-  window.location.reload();
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+
+  document.querySelector('.message').textContent = 'Start guessing again...!';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+
+  document.querySelector('.number').style.width = '15rem';
+
+//   window.location.reload();
 });
 /*  */
