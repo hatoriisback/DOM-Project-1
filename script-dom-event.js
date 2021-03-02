eb58694f6dc716d9ce27c438bf2043c7060cdab9
hendhendy <hendy.maxv@gmail.com>
2021-03-02 14:46:59 +0700

    Update script-dom-event.js

--- script-dom-event.js
+++ script-dom-event.js
@@ -13,68 +13,130 @@
 
 // // Handling Click Events & Implementing the Game Logic & Manipulating CSS Style // //
 
+// let secretNumber = Math.trunc(Math.random() * 20) + 1;
+// let score = 20;
+// let highscore = 0;
+
+// document.querySelector('.number').textContent = secretNumber;
+
+// document.querySelector('.check').addEventListener('click', function () {
+//   const guess = Number(document.querySelector('.guess').value);
+
+//   console.log(guess, typeof guess);
+
+//   // When there is no input
+//   if (!guess) {
+//     document.querySelector('.message').textContent = 'MASUKIN ANGKA BOS!🚫';
+
+//     // When the Player Wins
+//   } else if (guess === secretNumber) {
+//     document.querySelector('.message').textContent =
+//       'Tumben pintar, TEBAK LAGI YUK😛';
+//     document.querySelector('.number').textContent = secretNumber;
+
+//     document.querySelector('body').style.backgroundColor = '#60b347';
+//     document.querySelector('.number').style.width = '30rem';
+
+//     if (score > highscore) {
+//         highscore = score;
+//         document.querySelector('.highscore').textContent = highscore;
+//     }
+
+//     // When the Guess is too High
+//   } else if (guess > secretNumber) {
+//     if (score > 1) {
+//       document.querySelector('.message').textContent = '📈 Ketinggian woi!';
+//       score--;
+//       document.querySelector('.score').textContent = score;
+//     } else {
+//       document.querySelector('.message').textContent = 'Kurang Beruntung!😣';
+//       document.querySelector('.number').textContent = secretNumber;
+//       document.querySelector('.score').textContent = 0;
+//     }
+
+//     // When the Guess is too Low
+//   } else if (guess < secretNumber) {
+//     if (score > 1) {
+//       document.querySelector('.message').textContent = '📉 Kerendahan woi!';
+//       score--;
+//       document.querySelector('.score').textContent = score;
+//     } else {
+//       document.querySelector('.message').textContent = 'Kurang Beruntung!😣';
+//       document.querySelector('.number').textContent = secretNumber;
+//       document.querySelector('.score').textContent = 0;
+//     }
+//   }
+// });
+
+// // // Reload Pages
+// document.querySelector('.again').addEventListener('click', function () {
+//   score = 20;
+//   secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+//   document.querySelector('.message').textContent = 'Start guessing again...!';
+//   document.querySelector('.score').textContent = score;
+//   document.querySelector('.number').textContent = '?';
+//   document.querySelector('.guess').value = '';
+
+//   document.querySelector('body').style.backgroundColor = '#222';
+
+//   document.querySelector('.number').style.width = '15rem';
+// });
+//   window.location.reload();
+/*  */
+
+// // Refactoring Code - Stay DRY // //
+'use strict';
+
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
 
 document.querySelector('.number').textContent = secretNumber;
 
+const displayMessage =  function (message) {
+    document.querySelector('.message').textContent = message;
+}
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
-  console.log(guess, typeof guess);
-
   // When there is no input
   if (!guess) {
-    document.querySelector('.message').textContent = 'MASUKIN ANGKA CUK!🚫';
+      displayMessage('MASUKIN ANGKA BOS!🚫')
 
     // When the Player Wins
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent =
-      'Tumben pintar, TEBAK LAGI YUK😛';
+    displayMessage('Tumben pintar, TEBAK LAGI YUK😛')
     document.querySelector('.number').textContent = secretNumber;
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
 
     if (score > highscore) {
-        highscore = score;
-        document.querySelector('.highscore').textContent = highscore;
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
     }
-
-    // When the Guess is too High
-  } else if (guess > secretNumber) {
+    // When Guess is Wrong
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = '📈 Ketinggian woi!';
+      displayMessage(guess > secretNumber ? '📈 Ketinggian woi!' : '📉 Kerendahan woi!')
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = 'Kurang Beruntung!😣';
-      document.querySelector('.number').textContent = secretNumber;
-      document.querySelector('.score').textContent = 0;
-    }
-
-    // When the Guess is too Low
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '📉 Kerendahan woi!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = 'Kurang Beruntung!😣';
+      displayMessage('Kurang Beruntung!😣')
       document.querySelector('.number').textContent = secretNumber;
       document.querySelector('.score').textContent = 0;
     }
   }
 });
 
-// // Reload Pages
-document.querySelector('.again').addEventListener('click', function () {
+
+  document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-
-  document.querySelector('.message').textContent = 'Start guessing again...!';
+  displayMessage('Start guessing again...!')
   document.querySelector('.score').textContent = score;
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '';
@@ -82,7 +144,5 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('body').style.backgroundColor = '#222';
 
   document.querySelector('.number').style.width = '15rem';
-
-//   window.location.reload();
+  
 });
-/*  */
